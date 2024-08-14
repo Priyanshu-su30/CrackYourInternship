59c6be952bffd9b50c9671f7197bae77ced9a512
Priyanshu-su30 <107869522+Priyanshu-su30@users.noreply.github.com>
2024-08-14 23:28:20 +0530

    Create Sum_of_Left_Leaves.cpp

---
+++ Sum_of_Left_Leaves.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int sumOfLeftLeaves(TreeNode* root) {
+        if (root == nullptr)
+            return 0;
+        int sum = 0;
+        if (root->left != nullptr) {
+            if (root->left->left == nullptr && root->left->right == nullptr) {
+                sum += root->left->val;
+            } else {
+                sum += sumOfLeftLeaves(root->left);
+            }
+        }
+        sum += sumOfLeftLeaves(root->right);
+        return sum;
+    }
+};
